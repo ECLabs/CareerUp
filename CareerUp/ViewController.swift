f5bfd67e52351686b8165726910d7a64cdb077c8
Adam Emery <aqemery@gmail.com>
2014-09-30 10:22:23 -0400

    Initial code shell commit

---
+++ CareerUp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CareerUp
+//
+//  Created by Adam Emery on 9/30/14.
+//  Copyright (c) 2014 Adam Emery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
