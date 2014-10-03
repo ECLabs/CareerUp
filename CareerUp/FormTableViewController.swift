e5a6bb1f48b8ee1ac75cc8e0d82b0323edd0fecf
Adam Emery <aqemery@gmail.com>
2014-10-03 15:04:15 -0400

    Updated form view and image capture

---
+++ CareerUp/FormTableViewController.swift
@@ -0,0 +1,67 @@
+//
+//  FormTableViewController.swift
+//  CareerUp
+//
+//  Created by Adam Emery on 10/3/14.
+//  Copyright (c) 2014 Adam Emery. All rights reserved.
+//
+
+import UIKit
+
+class FormTableViewController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet var firstName:UITextField?
+    @IBOutlet var LastName:UITextField?
+    @IBOutlet var email:UITextField?
+    @IBOutlet var jobTitle:UITextField?
+    @IBOutlet var linkedIn:UITextField?
+    @IBOutlet var comments:UITextView?
+    @IBOutlet var resume:UIImageView?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    @IBAction func cancelTapped(AnyObject) {
+        println("hide form")
+        self.dismissViewControllerAnimated(true, completion: {})
+    }
+    
+    @IBAction func submitTapped(AnyObject) {
+        if(email!.text.isEmpty) {
+        
+            self.tableView.scrollRectToVisible(CGRectMake(0, 0, 1, 1), animated: true)
+        
+            let emailMissingAlert = UIAlertView(title: "Missing Required Field",
+                                                message: "Please enter your email address before submitting",
+                                                delegate: nil, cancelButtonTitle: "Ok")
+            emailMissingAlert.show()
+            
+            
+        } else {
+            println("submitting form")
+        }
+    }
+    
+    
+    @IBAction func cameraTapped(AnyObject) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = UIImagePickerControllerSourceType.Camera
+        self.presentViewController(imagePicker, animated: true, completion: {})
+    }
+    
+    
+    func imagePickerController(picker: UIImagePickerController!, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
+        resume!.image = image
+        
+        picker.dismissViewControllerAnimated(true, completion: {})
+    }
+    
+}
+
