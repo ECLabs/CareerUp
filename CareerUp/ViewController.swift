3f319016274554938ae3433ec2ae0d5a6f1544a2
Adam Emery <aqemery@gmail.com>
2014-10-15 12:06:36 -0400

    map background and fullscreen

--- CareerUp/ViewController.swift
+++ CareerUp/ViewController.swift
@@ -7,14 +7,34 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController, UINavigationControllerDelegate {
+    var map:MKMapView?
+    var overlayButton:UIButton?
+    var showMap = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController!.delegate = self
+        
+        map = MKMapView(frame: self.view.frame)
+        self.view.insertSubview(map!, atIndex: 0)
+        
+        
+        overlayButton = UIButton(frame: self.view.frame)
+        self.view.insertSubview(overlayButton!, atIndex: 1)
+        overlayButton?.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.3)
+        
+        overlayButton?.addTarget(self, action: "toggleFullscreenMap", forControlEvents: UIControlEvents.TouchUpInside)
+    }
+    
+    override func viewWillTransitionToSize(size: CGSize, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
+        map?.frame = CGRectMake(0, 0, size.width, size.height)
+        overlayButton?.frame = CGRectMake(0, 0, size.width, size.height)
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -25,6 +45,31 @@ class ViewController: UIViewController, UINavigationControllerDelegate {
         let hideNav = (viewController == self)
         
         navigationController.setNavigationBarHidden(hideNav, animated: true)
+        
+    }
+    
+    @IBAction func toggleFullscreenMap(){
+        println("fullscreen map")
+        self.navigationController?.setNavigationBarHidden(showMap, animated: true)
+        
+        UIView.animateWithDuration(1, animations: {
+            for object in self.view.subviews {
+            
+                if object_getClassName(object) == object_getClassName(self.map){
+                    continue
+                }
+                
+                let subview = object as UIView
+
+                if self.showMap {
+                    subview.alpha = 1
+                }
+                else {
+                    subview.alpha = 0
+                }
+            }
+        })
+        showMap = !showMap
         
     }
 }
